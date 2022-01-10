cf2dee2f494c71d313803944464d868ffde815b5
awsdevops1234567 <91582898+awsdevops1234567@users.noreply.github.com>
2022-01-10 17:26:47 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -4,5 +4,5 @@ sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
